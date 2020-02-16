b598f156d0283442516d1bd02b48d27ac1a912c4
marzam <zamith.marcelo@gmail.com>
2020-02-16 11:03:57 -0300

    first commit

---
+++ main-extract.py
@@ -0,0 +1,9 @@
+import pandas as pd
+#data = pd.read_csv("/home/mzamith/temp/Uso_do_Solo_2018.csv")
+#print(data.head())
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
